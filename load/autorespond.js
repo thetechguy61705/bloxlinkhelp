f14119809f346a2665d31dafffda640d06a665de
ethanlaj <ethanlaj@ericlaj.com>
2018-07-01 19:48:03 -0400

    ignore staff bc no1 loves them

--- load/autorespond.js
+++ load/autorespond.js
@@ -1,7 +1,8 @@
+const isStaff = require("app/staff");
 module.exports.run = async (bot) => {
 	var responses = bot.responses;
 	bot.on("message", (message) => {
-		if (message.author.bot || !message.content || message.channel.name !== "support") return;
+		if (message.author.bot || !message.content || message.channel.name !== "support" || isStaff(message.member));
 		for (let response of responses) {
 			for (let keyword of response.keywords) {
 				if (message.content.toLowerCase().includes(keyword)) {
